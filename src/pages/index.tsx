9d5c56918f90322bb851eb95177fb2d63266ceb7
ryone9re <ryoichi.kurimoto@gmail.com>
2022-06-23 07:54:50 +0900

    Initial commit from Create Next App

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@mui/material';
+
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Button variant='contained'>Hello World</Button>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,6 @@
+import { createTheme } from '@mui/material/styles';
+
+// TODO Create a theme instance.
+const theme = createTheme({});
+
+export default theme;
